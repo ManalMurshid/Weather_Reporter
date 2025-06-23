00e244e9458d9997fedf2420313aeb0f9d46290c
ManalMurshid <manalmurshid99@gmail.com>
2025-06-23 11:13:04 +0530

    Add deployment paths

--- Weather_App/vite.config.js
+++ Weather_App/vite.config.js
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  
+  // eslint-disable-next-line no-undef
+  const env = loadEnv(mode, process.cwd(), '');
+
+
+  return {
+    plugins: [react()],
+    base: env.VITE_BASE_PATH || "/Weather_Reporter",
+  }
 })
+
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: import.meta.env.VITE_BASE_PATH || "/Weather_Reporter",
+
+// })
