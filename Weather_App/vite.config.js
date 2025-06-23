b7a02e791fa48350d14a6d11d74e733ea13ddda9
ManalMurshid <manalmurshid99@gmail.com>
2025-06-23 15:14:05 +0530

    Fix subfolder error

--- Weather_App/vite.config.js
+++ Weather_App/vite.config.js
@@ -1,14 +1,27 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
+// Weather_App/vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  
-  // eslint-disable-next-line no-undef
-  const env = loadEnv(mode, process.cwd(), '');
-
+export default defineConfig(() => {
+  // const env = loadEnv(mode, process.cwd(), '');
   return {
     plugins: [react()],
-    base: env.VITE_BASE_PATH || "/Weather_Reporter",
-  }
-})
+    base: '/', // 👈 very important
+  };
+});
+
+
+// import { defineConfig, loadEnv } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig(({ mode }) => {
+  
+//   // eslint-disable-next-line no-undef
+//   const env = loadEnv(mode, process.cwd(), '');
+
+//   return {
+//     plugins: [react()],
+//     base: env.VITE_BASE_PATH || "/Weather_Reporter",
+//   }
+// });
